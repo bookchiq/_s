976bff635c76021d557a165ddefae152e42085f2
Sarah Lewis <sarah@happyhumans.com>
2015-02-23 09:01:26 -0800

    Improve get_secondary_content() function

--- inc/metaboxes.php
+++ inc/metaboxes.php
@@ -39,15 +39,42 @@ add_filter( 'cmb_meta_boxes', '_s_metaboxes' );
 
 /**
  * Retrieve a value from the metaboxes and display it
- * @param  string  $meta_name The id of the field
- * @param  boolean $wpautop   Whether or not to use the wpautop filter
  */
-function _s_get_secondary_content( $meta_name, $wpautop = true ) {
-	global $post;
+function _s_get_secondary_content( $args ) {
+	$defaults = array(
+		'id'      => null,  // The id of the custom field, without the prefix
+		'wpautop' => false, // Whether or not to use the wpautop filter
+		'before'  => null,
+		'after'   => null,
+		'wrapper' => 'div',
+		'classes' => array(),
+		'echo'    => true,
+	);
+	$args = wp_parse_args( $args, $defaults );
 
-	if ( $meta_content = get_post_meta( $post->ID, _S_METABOX_PREFIX . $meta_name, true ) ) {
-		echo '<div class="secondary-content secondary-content-' . $meta_name . '" >' . "\r\n";
-		echo ( $wpautop ) ? wpautop( $meta_content ) : $meta_content;
-		echo '</div><!-- .secondary-content-' . $meta_name . ' -->' . "\r\n";
+	global $post;
+	$out = '';
+
+	if ( $meta_content = get_post_meta( $post->ID, _S_METABOX_PREFIX . $args['id'], true ) ) {
+		$args['classes'][] = 'secondary-content';
+		$args['classes'][] = 'secondary-content-' . $args['id'];
+
+		$out .= '<' . $args['wrapper'] . ' class="' . implode( ' ', $args['classes'] ) . '" >' . PHP_EOL;
+
+		if ( $args['before'] ) {
+			$out .= '<span class="before">' . $args['before'] . '</span>';
+		}
+		$out .= '<span class="content">';
+		$out .= ( $args[ 'wpautop' ] ) ? wpautop( $meta_content ) : $meta_content;
+		$out .= '</span>';
+		if ( $args['after'] ) {
+			$out .= '<span class="after">' . $args['after'] . '</span>';
+		}
+		$out .= '</' . $args['wrapper'] . '><!-- .secondary-content-' . $args['id'] . ' -->' . PHP_EOL;
+	}
+	if ( $args[ 'echo' ] ) {
+		echo $out;
+	} else {
+		return $out;
 	}
 }
