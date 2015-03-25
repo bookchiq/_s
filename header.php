b419bc34c7be0c82cf900a29c1837d827ed82cd6
Sarah Lewis <sarah@happyhumans.com>
2015-03-25 14:20:34 -0700

    Change menu toggle wording to reflect current state

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 			</div>
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Menu', '_s' ); ?></button>
+				<button class="menu-toggle"><?php _e( 'Show Menu', '_s' ); ?></button>
 				<?php wp_nav_menu( array( 
 					'theme_location'  => 'primary',
 					'container_class' => 'primary-menu',
--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,12 @@
 svgeezy.init( 'nocheck', 'png' );
 
-// jQuery( document ).ready( function( $ ) {
-
-// } );
+jQuery( document ).ready( function( $ ) {
+	// Change menu toggle wording to reflect current state
+	$( '.menu-toggle' ).click( function(event) {
+		if ( 'Show Menu' == $( this ).text() ) {
+			$( this ).text( 'Hide Menu' );
+		} else if ( 'Hide Menu' == $( this ).text() ) {
+			$( this ).text( 'Show Menu' );
+		}
+	});
+} );
