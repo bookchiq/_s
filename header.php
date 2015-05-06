fc3976db1a1c6f9f26aa0d65b664f8390d113783
Sarah Lewis <sarah@happyhumans.com>
2015-05-06 11:05:11 -0700

    Make room for the favicon

--- header.php
+++ header.php
@@ -9,17 +9,18 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
-<!--[if (gte IE 6)&(lte IE 8)]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/selectivizr-ck.js" type="text/javascript"></script>
-<![endif]-->
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv-ck.js" type="text/javascript"></script>
-<![endif]-->
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+	<!--[if (gte IE 6)&(lte IE 8)]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/selectivizr-ck.js" type="text/javascript"></script>
+	<![endif]-->
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv-ck.js" type="text/javascript"></script>
+	<![endif]-->
+	<!-- <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" /> -->
 </head>
 
 <body <?php body_class(); ?>>
